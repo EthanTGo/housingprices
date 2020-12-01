e83309b05fe0fcd31aeb021ffa074f71c5ff2ab2
EthanTGo <ethango1997@gmail.com>
2020-12-01 00:11:57 -0500

    Added the project folder and reorganized structure of project

---
+++ Project/project.R
@@ -0,0 +1,19 @@
+getwd()
+
+setwd("/Users/ethango/Desktop/CS555/CS555-project")
+
+housing_data = read.csv('housing.csv')
+
+# Goal
+library(tidyverse)
+head(housing_data)
+# Dropping Columns
+
+
+glimpse(housing_data)
+
+
+
+
+
+
